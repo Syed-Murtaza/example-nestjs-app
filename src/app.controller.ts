2112cb3203c130fddf610f300f209b0b870eed6f
Syed-Murtaza <87502289+Syed-Murtaza@users.noreply.github.com>
2021-07-16 03:53:33 +0500

    Update app.controller.ts added 200 status

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @HttpCode(200)
   getHello(): string {
     return this.appService.getHello();
   }
